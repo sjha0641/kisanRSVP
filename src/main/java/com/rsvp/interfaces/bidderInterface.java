d264f16c0d94609494b1b64bd54eac4252f04d4b
Vishal Dubey <Vishal.Dubey@lntinfotech.com>
2019-12-20 04:55:16 +0530

    basic0.0.0.1

--- src/main/java/com/rsvp/interfaces/bidderInterface.java
+++ src/main/java/com/rsvp/interfaces/bidderInterface.java
@@ -1,12 +1,12 @@
 package com.rsvp.interfaces;
 
+import com.rsvp.entity.Bidder;
 import com.rsvp.entity.Crop;
-import com.rsvp.entity.Farmer;
 
 public interface bidderInterface {
 
 	public void addBidder();
-	public Farmer loginBidder(String email,String password);
+	public Bidder loginBidder(String email,String password);
 	public Crop fetchAllCrops();
 	public Crop placeABidByBidderIdAndCropId(int bidderId,int cropId);
 }
