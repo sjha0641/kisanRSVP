431037b8be31ce3268df8e266cc28e98dbff4d9f
Vishal Dubey <Vishal.Dubey@lntinfotech.com>
2019-12-20 04:52:33 +0530

    basic

--- src/main/java/com/rsvp/interfaces/adminInterface.java
+++ src/main/java/com/rsvp/interfaces/adminInterface.java
@@ -1,9 +1,10 @@
 package com.rsvp.interfaces;
 
+import com.rsvp.entity.Admin;
 import com.rsvp.entity.Farmer;
 
 public interface adminInterface {
 
 	public void addAdmin();
-	public Farmer loginAdmin(String email,String password);
+	public Admin loginAdmin(String email,String password);
 }
