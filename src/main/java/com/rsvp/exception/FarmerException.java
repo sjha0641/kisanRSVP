50d38d32b50846ae1f1e79a54cfcd8d12fcf0ce4
RaviThejaReddy <ravitheja.annapureddy@lntinfotech.com>
2019-12-21 14:33:52 +0530

    added exception

---
+++ src/main/java/com/rsvp/exception/FarmerException.java
@@ -0,0 +1,29 @@
+package com.rsvp.exception;
+
+public class FarmerException extends Exception {
+
+	public FarmerException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public FarmerException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public FarmerException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public FarmerException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public FarmerException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+}
